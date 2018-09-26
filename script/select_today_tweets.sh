7dc6af692aec14305cf7ce0213eaacaeeddb4d2f
cvaldex <cvaldex@gmail.com>
2018-09-26 18:58:41 -0300

    Carga inicial

---
+++ script/select_today_tweets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ../pass/password.txt #TO-DO Esto hay que encriptarlo
+source ../config/config.txt
+
+echo "Copying today Tweets..."
+
+export PGPASSFILE=../pgpass/.pgpass
+export TODAY_DATE=$(date +"%Y-%m-%d")
+
+psql -h $DB_HOST -U $DB_USER -d $DB_DBNAME -v a_date=\'$TODAY_DATE\' --file ../sql/insert_today_tweets.sql
+
+echo "Copy process finished!"
---
+++ sql/insert_today_tweets.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.today_tweets (tweet, eventdate, author, image1, image2, image3, image4, id)
+(
+	SELECT tweet, eventdate, author, image1, image2, image3, image4, id FROM public.tweets
+	WHERE  Extract(month from eventdate) = Extract(month from to_date(:a_date, 'YYYY-MM-DD'))
+	AND    Extract(day from eventdate) = Extract(day from to_date(:a_date, 'YYYY-MM-DD'))
+)

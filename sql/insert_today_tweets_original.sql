67dac7f85c93f1c0eb3f0dce3794f0dc3201b603
cvaldex <cvaldex@gmail.com>
2018-09-26 18:59:18 -0300

    Carga inicial

--- sql/insert_today_tweets_original.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.today_tweets (tweet, eventdate, author, image1, image2, image3, image4, id)
-(
-	SELECT tweet, eventdate, author, image1, image2, image3, image4, id FROM public.tweets
-	WHERE  Extract(month from eventdate) = Extract(month from to_date('2018-09-26', 'YYYY-MM-DD'))
-	AND    Extract(day from eventdate) = Extract(day from to_date('2018-09-26', 'YYYY-MM-DD'))
-)

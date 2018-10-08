f223d5ae40864b0b2d32cec10df286d842d3cae7
cvaldex <cvaldex@gmail.com>
2018-10-08 11:17:54 -0300

    Mejorando log Select Today Tweets

--- script/select_today_tweets.sh
+++ script/select_today_tweets.sh
@@ -9,9 +9,12 @@ echo "Copying today Tweets..."
 
 export PGPASSFILE=../pgpass/.pgpass #TO-DO Esto hay que encriptarlo
 export TODAY_DATE=$(date +"%Y-%m-%d")
+CURRENT_DATE=$(date +"%Y%m%d_%H%M%S")
+LOCAL_PATH=$HOME/rockHolidaysBashScript
+LOG_FILE=$LOCAL_PATH/log/$CURRENT_DATE.txt
 
-psql -h $DB_HOST -U $DB_USER -d $DB_DBNAME -v a_date=\'$TODAY_DATE\' --file ../sql/insert_today_tweets.sql
+psql -h $DB_HOST -U $DB_USER -d $DB_DBNAME -v a_date=\'$TODAY_DATE\' --file ../sql/insert_today_tweets.sql >> $LOG_FILE
 
-echo "Proceso de copiado de registros a tabla diaria completado" | mail -s "Movimiento de registros a tabla diaria $CURRENT_DATE" todayinrockhistorytwitter@gmail.com
+cat $LOG_FILE | mail -s "Movimiento de registros a tabla diaria $CURRENT_DATE" todayinrockhistorytwitter@gmail.com
 
 echo "Copy process finished!"

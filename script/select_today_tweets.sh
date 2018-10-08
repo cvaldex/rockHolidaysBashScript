2e9658730cd85b790cdc11c890922a9a8b8cd40a
cvaldex <cvaldex@gmail.com>
2018-10-07 23:25:22 -0300

    Se envia correo en proceso de copia diaria

--- script/select_today_tweets.sh
+++ script/select_today_tweets.sh
@@ -9,4 +9,6 @@ export TODAY_DATE=$(date +"%Y-%m-%d")
 
 psql -h $DB_HOST -U $DB_USER -d $DB_DBNAME -v a_date=\'$TODAY_DATE\' --file ../sql/insert_today_tweets.sql
 
+echo "Proceso de copiado de registros a tabla diaria completado" | mail -s "Movimiento de registros a tabla diaria $CURRENT_DATE" todayinrockhistorytwitter@gmail.com
+
 echo "Copy process finished!"

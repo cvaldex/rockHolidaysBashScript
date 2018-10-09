911d9b0bd9c4d29846dc045f855671667954ed46
cvaldex <cvaldex@gmail.com>
2018-10-09 18:36:51 -0300

    Cambio en la query - Permitiendo movimiento incremental

--- sql/insert_today_tweets.sql
+++ sql/insert_today_tweets.sql
@@ -3,4 +3,5 @@ INSERT INTO public.today_tweets (tweet, eventdate, author, image1, image2, image
 	SELECT tweet, eventdate, author, image1, image2, image3, image4, id FROM public.tweets
 	WHERE  Extract(month from eventdate) = Extract(month from to_date(:a_date, 'YYYY-MM-DD'))
 	AND    Extract(day from eventdate) = Extract(day from to_date(:a_date, 'YYYY-MM-DD'))
+	AND id NOT IN (SELECT DISTINCT id FROM public.today_tweets)
 )

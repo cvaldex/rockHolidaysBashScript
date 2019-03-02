ce2ff8c1a78c532b1be946f251d141cfc3eb7ab2
cvaldex <cvaldex@gmail.com>
2019-03-01 23:04:20 -0300

    Se agrega el campo prioridad desde la tabla tweets a today_tweets

--- sql/insert_today_tweets.sql
+++ sql/insert_today_tweets.sql
@@ -1,6 +1,6 @@
-INSERT INTO public.today_tweets (tweet, eventdate, author, image1, image2, image3, image4, id)
+INSERT INTO public.today_tweets (tweet, eventdate, author, priority, image1, image2, image3, image4, id)
 (
-	SELECT tweet, eventdate, author, image1, image2, image3, image4, id FROM public.tweets
+	SELECT tweet, eventdate, author, priority, image1, image2, image3, image4, id FROM public.tweets
 	WHERE  Extract(month from eventdate) = Extract(month from to_date(:a_date, 'YYYY-MM-DD'))
 	AND    Extract(day from eventdate) = Extract(day from to_date(:a_date, 'YYYY-MM-DD'))
 	AND id NOT IN (SELECT DISTINCT id FROM public.today_tweets)
